bf8605b92a192bcfa20005a2671f7073725336a5
riyaz <riyazdutta115@gmail.com>
2026-03-22 09:03:58 +0000

    datatypes done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,2 +1,32 @@
 "user strict"; // treat all JS Code as newer version 
  
+// alert (3+3)  // we are using nodejs, not browser
+
+
+let name = "kai"
+let age = 20
+let isLoggedIn = false
+
+
+
+// Number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => not assigned value yet
+// symbol => unique 
+
+
+// object
+
+ console.log(typeof age);
+
+
+/*
+null is an objeect , undefined is a type itself
+*/
+
+
+console.log(typeof undefined); // undefined
+ console.log(typeof null); // object
