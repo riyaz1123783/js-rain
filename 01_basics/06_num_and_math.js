f40986df116aebe28dcab87710318a3604cbb058
riyaz <riyazdutta115@gmail.com>
2026-04-20 21:24:50 +0000

    done 21-04-2026

--- 01_basics/06_num_and_math.js
+++ 01_basics/06_num_and_math.js
@@ -1,18 +1,18 @@
-const score = 400
-console.log(score);
+// const score = 400
+// console.log(score);
 
-const balance = new Number(100)
-console.log(balance);
+// const balance = new Number(100)
+// console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
 
-const otherNumber = 23.8966
+// const otherNumber = 23.8966
 
-console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(4));
 
-const hundreds = 10000000
-console.log(hundreds.toLocaleString('en-IN'));
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
 
 
 /*
@@ -22,4 +22,42 @@ explian all topics in details and add ouputs
 
 
 //++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 5, 3, 8, 9));
+// console.log(Math.max(8, 6 ,1 ,7 , 9));
+
+/*
+explain each topic thoroughly with proper and easy definations
+*/
+
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+
+
+/*
+outputs and explainations 
+*/
+
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min +1)) +min);
+
+
+/*
+deep explainations and outputs 
+*/
+
 
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,6 +2,9 @@ console.log("Kai")
 
 
 
+
+
+
  
 
 
