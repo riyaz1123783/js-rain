537127227f349c296ea474ff1aef32ddc32bc72f
riyaz <riyazdutta115@gmail.com>
2026-05-09 20:43:37 +0000

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+// Arrray
+
+const myArr = [0, 1, 2, 3, 4, 5, "kai", true]
+
+const pNames = ["kcl", "b21", "fzf"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[3]);
+
+
+
+// Array Methods
+
+// myArr.push(9)
+// console.log(myArr); // must  add output with reasons why it gave you that output 
+
+// myArr.pop()
+// console.log(myArr); // must  add output with reasons why it gave you that output 
+
+myArr.unshift(7)
+myArr.shift()
+console.log(myArr); // must  add output with reasons why it gave you that output 
+
+
+console.log(myArr.includes(7));
+console.log(myArr.indexOf(3));
+
+
